2c28e8c4075580a12c638f2fb5f27564cd47ccaa
Andrew Benson <abenson@obs.carnegiescience.edu>
2025-09-15 07:58:29 -0700

    fix: Remove code that handles ignoring whitespace changes - this is now directly implemented in the parameter migration script

--- migrateAllParameterFiles.pl
+++ migrateAllParameterFiles.pl
@@ -34,36 +34,9 @@ sub runMigrations {
     my $doc   = $parser->parse_file($fileName);
     return
 	unless ( $doc->findnodes('//parameters') );    
-    # Pre-process the file to concatenate any attribute values that are split across multiple lines.
-    open(my $fileInput ,"<",$fileName       );
-    open(my $fileOutput,">",$fileName.".tmp");
-    my $inMultiline = 0;
-    while ( my $line = <$fileInput> ) {
-	my $countQuotes = $line =~ tr/"//;
-	$inMultiline = 1-$inMultiline
-	    if ( $countQuotes % 2 == 1 );
-	$line =~ s/\n/\%\%NEWLINE\%\%/
-	    if ( $inMultiline );
-	print $fileOutput $line;
-    }
-    close($fileInput );
-    close($fileOutput);
     # Migrate the parameter file.
-    system("cd ".$ENV{'GALACTICUS_EXEC_PATH'}."; ./scripts/aux/parametersMigrate.pl ".$File::Find::name.".tmp migration__.xml.tmp --validate no --timeStamp ".$timeStamp);
-    # Make a patch from the old to the new file, but ignoring changes in whitespace.
-    system("cd ".$ENV{'GALACTICUS_EXEC_PATH'}."; diff -w -p ".$File::Find::name.".tmp migration__.xml.tmp > tmp__.patch");
-    # Apply the patch to the old file - we now have migrations applied, but no change in whitespace formatting.
-    system("cd ".$ENV{'GALACTICUS_EXEC_PATH'}."; patch ".$File::Find::name.".tmp tmp__.patch");
-    # Undo any split line reformatting that we previously applied.
-    open(my $migratedFileInput ,"<",$fileName.".tmp");
-    open(my $migratedFileOutput,">",$fileName       );
-    while ( my $line = <$migratedFileInput> ) {
-	$line =~ s/\%\%NEWLINE\%\%/\n/g;
-	print $migratedFileOutput $line;
-    }
-    close($migratedFileInput );
-    close($migratedFileOutput);
+    system("cd ".$ENV{'GALACTICUS_EXEC_PATH'}."; ./scripts/aux/parametersMigrate.pl ".$File::Find::name." migration__.xml.tmp --ignoreWhiteSpaceChanges yes --validate no --timeStamp ".$timeStamp);
     # Clean up.
-    unlink($fileName.".tmp",$ENV{'GALACTICUS_EXEC_PATH'}."/tmp__.patch",$ENV{'GALACTICUS_EXEC_PATH'}."/migration__.xml.tmp");
+    mv($ENV{'GALACTICUS_EXEC_PATH'}."/migration__.xml.tmp",$File::Find::name);
     return;
 }
