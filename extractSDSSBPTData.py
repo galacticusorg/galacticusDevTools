c7f6c2192305ceb35da824009eeba54657bc483f
Andrew Benson <abenson@carnegiescience.edu>
2026-01-22 14:09:07 -0800

    feat: Add extract of SDSS AGN emission line fluxes

--- extractSDSSBPTData.py
+++ extractSDSSBPTData.py
@@ -41,25 +41,53 @@ linesFile = fits.open('galSpecLine-dr8.fits')
 lines     = linesFile[1].data
 
 # Select star forming galaxies.
-select = extra['bptclass'] == 1
+selectSF  = extra['bptclass'] == 1
 
-# Extract required lines.
-fluxes = {}
-fluxes['balmerAlpha6565'] = lines['h_alpha_flux'  ][select]
-fluxes['balmerBeta4863' ] = lines['h_beta_flux'   ][select]
-fluxes['oxygenII3727'   ] = lines['oii_3726_flux' ][select]
-fluxes['oxygenIII5008'  ] = lines['oiii_5007_flux'][select]
-fluxes['nitrogenII6585' ] = lines['nii_6584_flux' ][select]
-fluxes['sulfurII6718'   ] = lines['sii_6717_flux' ][select]
-fluxes['sulfurII6733'   ] = lines['sii_6731_flux' ][select]
+# Select AGN galaxies.
+selectAGN = extra['bptclass'] == 4
 
 # Get git revision.
 repo         = Repo(".")
 lastRevision = repo.head.object.hexsha
 
+# Star forming galaxies.
+
+# Extract required lines.
+fluxes = {}
+fluxes['balmerAlpha6565'] = lines['h_alpha_flux'  ][selectSF]
+fluxes['balmerBeta4863' ] = lines['h_beta_flux'   ][selectSF]
+fluxes['oxygenII3727'   ] = lines['oii_3726_flux' ][selectSF]
+fluxes['oxygenIII5008'  ] = lines['oiii_5007_flux'][selectSF]
+fluxes['nitrogenII6585' ] = lines['nii_6584_flux' ][selectSF]
+fluxes['sulfurII6718'   ] = lines['sii_6717_flux' ][selectSF]
+fluxes['sulfurII6733'   ] = lines['sii_6731_flux' ][selectSF]
+
 # Output to HDF5.
 output = h5py.File(os.environ.get('GALACTICUS_DATA_PATH')+'/static/observations/emissionLines/emissionLineFluxesStarFormingSDSSDR8.hdf5','w')
 output.attrs['description'         ] = "Emission line fluxes of star forming galaxies from the SDSS DR8 catalog."
+output.attrs['referenceURL'        ] = "https://www.sdss4.org/dr14/spectro/galaxy_mpajhu/"
+output.attrs['createdBy'           ] = "https://github.com/galacticusorg/galacticusDevTools/blob/"+lastRevision+"/extractSDSSBPTData.py"
+output.attrs['timeStamp'           ] = str(datetime.datetime.now())
+output.attrs['galSpecExtraChecksum'] = md5_checksum('galSpecExtra-dr8.fits')
+output.attrs['galSpecLineChecksum' ] = md5_checksum('galSpecLine-dr8.fits' )
+for lineName in fluxes:
+    output.create_dataset(lineName,data=fluxes[lineName])
+
+# AGN
+    
+# Extract required lines.
+fluxes = {}
+fluxes['balmerAlpha6565'] = lines['h_alpha_flux'  ][selectAGN]
+fluxes['balmerBeta4863' ] = lines['h_beta_flux'   ][selectAGN]
+fluxes['oxygenII3727'   ] = lines['oii_3726_flux' ][selectAGN]
+fluxes['oxygenIII5008'  ] = lines['oiii_5007_flux'][selectAGN]
+fluxes['nitrogenII6585' ] = lines['nii_6548_flux' ][selectAGN]
+fluxes['sulfurII6718'   ] = lines['sii_6717_flux' ][selectAGN]
+fluxes['sulfurII6733'   ] = lines['sii_6731_flux' ][selectAGN]
+
+# Output to HDF5.
+output = h5py.File(os.environ.get('GALACTICUS_DATA_PATH')+'/static/observations/emissionLines/emissionLineFluxesAGNSDSSDR8.hdf5','w')
+output.attrs['description'         ] = "Emission line fluxes of AGN from the SDSS DR8 catalog."
 output.attrs['referenceURL'        ] = "https://www.sdss4.org/dr14/spectro/galaxy_mpajhu/"
 output.attrs['createdBy'           ] = "https://github.com/galacticusorg/galacticusDevTools/blob/"+lastRevision+"/extractSDSSBPTData.py"
 output.attrs['timeStamp'           ] = str(datetime.datetime.now())
